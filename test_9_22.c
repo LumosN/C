eada3a073a198188c6e5a0d926d455562a14849e
LumosN <70007434+LumosN@users.noreply.github.com>
2020-09-23 22:31:22 +0800

    汉诺塔-递归

--- test_9_22.c
+++ test_9_22.c
@@ -1,35 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-//void towers(int n, char A, char B, char C)
-//{
-//	if (n == 1)
-//	{
-//		printf("move desk 1 from %c to %c\n", A , C);
-//	}
-//	towers(n - 1, A, B, C);
-//	printf("move desk %d from %c to %c", n, A, C);
-//	towers(n - 1, B, C, A);
-//}
-//int main()
-//{
-//	int n = 0;
-//	scanf("%d", &n);
-//	towers(n, 'A', 'C', 'B');
-//	return 0;
-//}
 void towers(int n, char A, char C, char B)
 {
-	if (n == 1)		//递归出口
+	if (n == 1)		//閫掑綊鍑哄彛
 	{
 		printf("move disk 1 from peg %c to peg %c\n",
 			A, C);
 		return;
 	}
-	//把n-1个圆盘从A借助C移至B
+	//鎶妌-1涓渾鐩樹粠A鍊熷姪C绉昏嚦B
 	towers(n - 1, A, B, C);
-	//把圆盘n由A直接移至C
+	//鎶婂渾鐩榥鐢盇鐩存帴绉昏嚦C
 	printf("move disk %d from peg %c to peg %c\n", n, A, C);
-	//把n-1个圆盘从B借助A移至C
+	//鎶妌-1涓渾鐩樹粠B鍊熷姪A绉昏嚦C
 	towers(n - 1, B, C, A);
 }
 int main()

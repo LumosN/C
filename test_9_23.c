bc24d5a38be2517e71c45fa007e61845c1a3b41f
LumosN <70007434+LumosN@users.noreply.github.com>
2020-09-24 22:37:05 +0800

    Update test_9_23.c

--- test_9_23.c
+++ test_9_23.c
@@ -3,19 +3,21 @@
 #include<assert.h>
 char* my_strcpy(char* dest, const char* src)
 {
+	char* ret = dest;
 	assert(dest != NULL);
 	assert(src != NULL);
 	while (*dest++ = *src++)//'\0'的ASCll码值是0
 	{
 		;
 	}
+	return dest;
 }
 int main()
 {
 	char arr1[20] = { 0 };
 	char arr2[20] = { 0 };
 	printf("输入字符串\n");
-	scanf("%s", arr1);
-	printf("%s", my_strcpy(arr2, arr1));//把arr1中的字符串copy到arr2中 
+	scanf("%s", &arr1);
+	printf("%s", my_strcpy(arr2, arr1));//把arr1中的字符串copy到arr2中  
 	return 0;
 }

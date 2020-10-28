b4cbd871a73289a2f9b938eecae462d394722dbb
LumosN <621034090@qq.com>
2020-10-28 21:45:08 +0800

    模拟实现库函数：strcmp

---
+++ test_10_28.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<assert.h>
+int my_strcmp(const char* s1, const char* s2)
+{
+	assert(s1);
+	assert(s2);
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return 0;
+		}
+		s1++;
+		s2++;
+	}
+	return *s1 - *s2;
+}
